c063133e32f6002cf99a2bc2ad9468c8d7d07807
rushi3691 <rushi3691@outlook.com>
2022-10-20 06:39:28 +0530

    commit message

--- eng_mar/settings.py
+++ eng_mar/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+6244q6+68sw+%=c24c=2rk$_bu((m)5w-6j3qhee8a&h0*g2$'
+DUM_KEY = 'django-insecure-+6244q6+68sw+%=c24c=2rk$_bu((m)5w-6j3qhee8a&h0*g2$'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', default=DUM_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
